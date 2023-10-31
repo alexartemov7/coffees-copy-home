5a15367360754bce684916a691f3e7f5845a5dc4
Alex Artemov <artemov.sandro@gmail.com>
2023-10-30 22:30:31 -0400

    created hero component and applied basic styling

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import './hero.css' 
+
+
+export default function Hero() { //creating and exporting function of the Hero component
+    return (
+        <header>
+          <h1>Super Coffee List</h1>
+        </header>
+    )
+}
